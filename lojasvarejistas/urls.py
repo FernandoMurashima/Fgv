e36a2d104cb9e9937c76353419154d33b36e1f53
Fernando Murashima <Fernando@Murashima.com.br>
2023-11-21 21:39:38 -0300

    Aula2

--- lojasvarejistas/urls.py
+++ lojasvarejistas/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from lojasvarejistasapp.views import home
+from lojasvarejistasapp.views import home, form
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('form/', form),
+
 
 ]
--- lojasvarejistasapp/views.py
+++ lojasvarejistasapp/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello World!')
+    return render(request, 'index.html')
+
+def form(request):
+    return render(request, 'form.html')
